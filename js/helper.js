c01cd3596b301072a6427fac1954345f5190250f
diogob <diogobranco94@gmail.com>
2022-10-02 18:09:24 +0100

    updated events

---
+++ js/helper.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    n_clubs = 3
+    var club = Math.floor(Math.random() * n_clubs);
+    showClubInfo(club);
+});
+
+/*
+club => the club name
+1 => football
+2 => running
+3 => tabletop
+defines the following fields:
+club_title
+club_text
+club_img
+club_date
+club_location
+club_form
+ */
+function showClubInfo(club = -1) {
+    switch (club) {
+        case 0:
+            $("#club_title").text("Futsal");
+            $("#club_text").text("");
+            $("#club_img").attr("src", "assets/img/events/sports.jpg");
+            $("#club_date").text("Thurdays at 6pm");
+            $("#club_location").text("Estádio Universitário de Lisboa");
+            $("#club_form").text("");
+
+            break;
+        case 1:
+            $("#club_title").text("Running");
+            $("#club_text").text("In the Ph.D., we run and exercise mentally to achieve our goals, right? But we also don't always want to be sedentary winners. We propose teambuilding sessions through marathons and running. <br>Why? 'Mens sana in corpore sano '");
+            $("#club_img").attr("src", "assets/img/events/running.jpg");
+            $("#club_date").text("");
+            $("#club_location").text("");
+            $("#club_form").text("");
+            break;
+        case 2:
+            $("#club_title").text("Tabletop");
+            $("#club_text").text("");
+            $("#club_img").attr("src", "assets/img/events/tabletop.jpg");
+            $("#club_date").text("");
+            $("#club_location").text("");
+            $("#club_form").text("");
+            break;
+
+        default:
+            break;
+    }
+
+}
