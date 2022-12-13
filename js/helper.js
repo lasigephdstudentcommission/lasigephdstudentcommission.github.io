043ec5d5b818f5ec4821756b381d89cc96d4b218
pedro <pgcotovio@gmail.com>
2022-12-13 19:36:16 +0000

    update forum / add tab system

--- js/helper.js
+++ js/helper.js
@@ -167,6 +167,9 @@ forum_location
 forum_form
  */
 function showForumInfo(forum = -1) {
+
+    sessionStorage.setItem("edition", forum);
+
     switch (forum) {
         case 0:
             $("#forum_title").text("LASIGE Forum");
@@ -179,25 +182,127 @@ function showForumInfo(forum = -1) {
             $("#more_form_text").text("Past event");
             $("#forum_form").text("");
 
+            $("#forum_tabs").html(``);
+            $("#forum_talks").html(``);
+
+
             break;
         case 1:
             $("#forum_title").text("LASIGE Forum");
             $("#forum_subtitle").text("2nd Edition");
             // $("#forum_text").text("");
-            $("#forum_img").attr("src", "assets/img/events/2nd-Forum-01.png");
+            $("#forum_img").attr("src", "assets/img/events/Forum2022/2nd-Forum-01.png");
             $("#forum_date").text("Date: December 16th 2022");
             $("#forum_location").text("Location: C6.2.56");
             $("#forum_more").attr("href", "https://discord.gg/n53AfFnRSn");
             $("#more_form_text").text("Registrations Closed");
             $("#forum_form").text(" ");
             $("#forum_form").attr("href", " ");
-            $("#forum_img_1").attr("src", "assets/img/events/Forum-keynote.png");
-            $("#forum_img_2").attr("src", "assets/img/events/Forum-panel.png");
-            $("#forum_img_3").attr("src", "assets/img/events/Forum-thesis.png");
+
+
+            $("#forum_tabs").html(`
+                                        <a href="" onclick="showForumImage(0); return false; "> Program</a>
+                                        <a href="" onclick="showForumImage(1); return false; "> Keynote</a>
+                                        <a href="" onclick="showForumImage(2); return false; "> Panel</a>
+                                        <a href="" onclick="showForumImage(3); return false; "> Talks</a>
+                                        <a href="" onclick="showForumImage(4); return false; "> Thesis Defense</a>
+                                        <a href="" onclick="showForumImage(5); return false; "> Lunch</a>
+                                `);
+
+             $("#forum_talks").html(``);
             break;
 
         default:
             break;
+    }
+
+}
+
+function showForumImage(forum = -1) {
+
+    edition = sessionStorage.getItem("edition");
+
+    switch (edition) {
+        case '0':
+            $("#forum_img").attr("src", "assets/img/events/Forum.png");
+        break;
+
+        case '1':
+
+            switch (forum) {
+                case 0:
+                    $("#forum_talks").html(``);
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/2nd-Forum-01.png");
+                    break;
+                case 1:
+                    $("#forum_talks").html(``);
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Forum-keynote.png");
+                    break;
+                case 2:
+                    $("#forum_talks").html(``);
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Forum-panel.png");
+                    break;
+                case 3:
+                    $("#forum_talks").html(`
+                    <a href="" onclick="showForumTalk(0); return false; "> Tiago Carvalho - 15:00</a>
+                    <a href="" onclick="showForumTalk(1); return false; "> Duarte Saraiva - 15:15</a>
+                    <a href="" onclick="showForumTalk(2); return false; "> Karina Brotto Rebuli - 15:30</a>
+                    <a href="" onclick="showForumTalk(3); return false; "> Pedro Pais - 15:45</a>
+                    `);
+                    break;
+        
+                case 4:
+                    $("#forum_talks").html(``);
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Forum-thesis.png");
+                    break;
+
+                case 5:
+                    $("#forum_talks").html(``);
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Menu.png");
+                    break;
+        
+                default:
+                    $("#forum_talks").html(``);
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/2nd-Forum-01.png");
+                    break;
+            }
+
+        break;
+
+    }
+
+}
+
+function showForumTalk(forum = -1) {
+
+    edition = sessionStorage.getItem("edition");
+
+    switch (edition) {
+
+
+        case '1':
+
+            switch (forum) {
+                case 0:
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Tiago.png");
+                    break;
+                case 1:
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Duarte.png");
+                    break;
+                case 2:
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Karina.png");
+                    break;
+                case 3:
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Pedro.png");
+                    break;
+        
+                default:
+                    $("#forum_img").attr("src", "assets/img/events/Forum2022/Tiago.png");
+                    break;
+            }
+
+        break;
+
     }
 
 }
